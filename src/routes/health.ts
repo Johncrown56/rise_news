8aff6662e4644e04d3ef7c3cb107dfb2e1cb2873
John Adepoju <adepojujohn56@gmail.com>
2024-10-13 17:57:21 +0100

    fix: Added health check 2

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -6,7 +6,7 @@ router.get("/health", (req: Request, res: Response, _next: NextFunction) => {
   res
     .status(200)
     .header("Content-Type", "text/html")
-    .send(`<h1>Asharami's APIs  are up and running</h1>`);
+    .send(`<h1>All APIs are up and running</h1>`);
 });
 
 export default router;
